7f539518295115cafa1540d558f3b0ac5ed216fd
Andrey Tremasov <tremasov123@gmail.com>
2021-07-18 14:22:37 +0300

    create class NeuralNetwork with prediction

--- main.py
+++ main.py
@@ -33,29 +33,50 @@ class Neuron:
         return activation(total)
 
 
+
+
+class NeuralNetwork:
+    def __init__(self, count_input, count_layers, count_neurons):
+        self.count_input = count_input
+        self.count_layers = count_layers
+        self.count_neurons = count_neurons
+        self.neural_network = []
+    
+        for i in range(count_layers):
+            layer = []
+            if i > 0:
+                count = count_neurons[i - 1]
+            else:
+                count = count_input
+            for num in range(count_neurons[i]):
+                weights = [np.random.normal() for k in range(count)]
+                bias = np.random.normal()
+                neuron1 = Neuron(weights, bias)
+                layer.append(neuron1)
+            self.neural_network.append(layer)
+
+
+    def feedforward(self, x):
+        values = np.array(x)
+        for i in range(count_layers - 1):
+            next_values = []
+            for j in range(count_neurons[i]):
+                value = self.neural_network[i][j].feedforward(values, relu)
+                next_values.append(value)
+            values = np.array(next_values)
+        return self.neural_network[count_layers - 1][0].feedforward(values, linear)
+
+
+
+
+
+
 count_input = 3
-
-data_in, data_out = read_data('in.txt', count_input)
 count_layers = 3
-
 count_neurons = [4, 3, 1]
-
-neural_network = []
-for i in range(count_layers):
-    layer = []
-    if i > 0:
-        count = count_neurons[i - 1]
-    else:
-        count = count_input
-    for num in range(count_neurons[i]):
-        weights = [np.random.normal() for k in range(count)]
-        bias = np.random.normal()
-        neuron1 = Neuron(weights, bias)
-        layer.append(neuron1)
-    neural_network.append(layer)
-
-
-
-
+data_in, data_out = read_data('in.txt', count_input)
+neural_network = NeuralNetwork(count_input, count_layers, count_neurons)
+prediction = neural_network.feedforward(data_in[0])
+print(prediction)
 
     
