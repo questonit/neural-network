249f55967391e043b13e0444331c22c2cc842c6e
Andrey Tremasov <tremasov123@gmail.com>
2021-07-18 13:52:58 +0300

    initialize neural network

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import NewType
 import numpy as np
 
 
@@ -18,22 +19,41 @@ def read_data(filename, width):
 def relu(x):
     return max(0, x)
 
+def linear(x):
+    return x
+
 
 class Neuron:
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
 
-    def feedforward(self, inputs):
+    def feedforward(self, inputs, activation):
         total = np.dot(self.weights, inputs) + self.bias
-        return relu(total)
+        return activation(total)
 
 
+count_input = 3
 
+data_in, data_out = read_data('in.txt', count_input)
+count_layers = 3
 
+count_neurons = [4, 3, 1]
 
+neural_network = []
+for i in range(count_layers):
+    layer = []
+    if i > 0:
+        count = count_neurons[i - 1]
+    else:
+        count = count_input
+    for num in range(count_neurons[i]):
+        weights = [np.random.normal() for k in range(count)]
+        bias = np.random.normal()
+        neuron1 = Neuron(weights, bias)
+        layer.append(neuron1)
+    neural_network.append(layer)
 
-data_in, data_out = read_data('in.txt', 3)
 
 
 
